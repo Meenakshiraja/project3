c711c42f238c4c6c7ce777f6e04a87e417de36c6
Meenashiraja <rajameenakshi3@gmail.com>
2022-11-10 18:15:17 +0530

    commit

--- src/app/studentlist/studentlist.component.ts
+++ src/app/studentlist/studentlist.component.ts
@@ -9,22 +9,27 @@ import { StudentdataService } from '../studentdata.service';
 })
 export class StudentlistComponent implements OnInit {
 
-  
-
   studentmark:{sno:number,roll:any,name:string,total:any,percentage:any,grade:string,status:string}[]=[];
 
   formdata:{roll:any,name:string,subject:any,practical:boolean,theory:number,practicalmark:number}[]=[];
   
-  maxtheory=100;
-  maxpractical=25;
-
-  studentform=new FormGroup({
+  show=false;
+     
+  studentform=new FormGroup
+  <{
+    roll:FormControl<string|null>,
+    name:FormControl<string|null>,
+    subject:FormControl<string|null>,
+    practical:FormControl<boolean|null>,
+    theory:FormControl<string|null>,
+    practicalmark?:FormControl<string|null>,
+  }>
+  ({
     roll:new FormControl('',Validators.compose([Validators.required,Validators.pattern('[0-9]{2}[a-zA-Z .]{3}[0-9]{2}')])),
     name:new FormControl('',Validators.compose([Validators.required,Validators.pattern('[a-zA-Z .]*')])),
     subject:new FormControl('',Validators.compose([Validators.required,Validators.pattern('[a-zA-Z0-9 .]*')])),
-    practical:new FormControl(''),
-    theory:new FormControl('',Validators.compose([Validators.required,Validators.pattern('[0-9-]*')])),
-    practicalmark:new FormControl({value:'0',disabled:true})
+    practical:new FormControl(false),
+    theory:new FormControl('',[Validators.required,Validators.max(100),Validators.pattern('[0-9-]*')]),
   });
   constructor(public studservice:StudentdataService) { }
 
@@ -36,31 +41,15 @@ export class StudentlistComponent implements OnInit {
   {
      if(this.studentform.valid)
      {
-       this.studentform.controls["practicalmark"].enable();
        alert("Submitted Successfully");
        this.studservice.saveformdata(this.studentform.value);
        this.studentform.controls["theory"].setValue('');
        this.studentform.controls["subject"].setValue('');
-       this.studentform.controls["practicalmark"].setValue('0');
-       this.studentform.controls["practicalmark"].disable();
+       this.studentform.controls["practical"].setValue(false);
+       this.show=false;
      }
   }
   
-  checkrange(e:any)
-  {
-    if(e.value>this.maxtheory)
-    {
-      if(this.maxtheory==75)
-      {
-        alert("Please enter the mark for 75");
-        e.value='';
-      }
-      else{
-        alert("Please enter the mark for 100");
-        e.value='';
-      }
-    }
-  }
   calc(a:any,b:any)
   {
     let c=(Number)(a)+(Number)(b);
@@ -69,23 +58,24 @@ export class StudentlistComponent implements OnInit {
   savestudent()
   {
     this.studservice.savestudent(this.studentform.value);
+    this.studentform.reset;
   }
 
   practicalchecked(e:any)
   {
     if(e.checked==true)
     {
-      this.maxtheory=75;
-      this.studentform.controls["practicalmark"].enable();
-      this.studentform.controls["practicalmark"].setValidators([Validators.required]);
+      this.show=true;
+      this.studentform.addControl("practicalmark",new FormControl('',[Validators.required,Validators.max(25)]));
+      this.studentform.controls["theory"].setValidators([Validators.required,Validators.max(75),Validators.pattern('[0-9-]*')]);
     }
     else{
-      this.maxtheory=100;
-      this.studentform.controls["practicalmark"].disable();
-      this.studentform.controls["practicalmark"].setValidators(null);
+      this.show=false;
+      this.studentform.removeControl("practicalmark");
+      this.studentform.controls["theory"].setValidators([Validators.required,Validators.max(100),Validators.pattern('[0-9-]*')]);
+      this.studservice.formdata.splice(5,1);
     }
   }
-
   // get f()
   // {return this.studentform.controls;}
   // In html access as *ngIf=f.name.errors.required
